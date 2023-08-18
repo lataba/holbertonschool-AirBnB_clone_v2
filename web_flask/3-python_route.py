b415b7c505d0a1e7e57aa2f55cc9c9fc6ac56559
lataba <ltballeste@gmail.com>
2023-08-18 11:47:54 -0700

    Tasks 4, 5

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Flask model for routes
+Create a Flask web application instance
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Route function for the root URL ('/') """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def first():
+    """ Route function for the root URL ('/hbnb') """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def second(text):
+    """ Route function for the root URL ('/c/<text>') """
+    txt = text.replace("_", " ")
+    return f'C {txt}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def tirth(text):
+    """ Route function for the root URL ('/python/<text>') """
+    txt = text.replace("_", " ")
+    return f'Python {txt}'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Flask model for routes
+Create a Flask web application instance
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Route function for the root URL ('/') """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def first():
+    """ Route function for the root URL ('/hbnb') """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def second(text):
+    """ Route function for the root URL ('/c/<text>') """
+    txt = text.replace("_", " ")
+    return f'C {txt}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def tirth(text = 'is cool'):
+    """ Route function for the root URL ('/python/<text>') """
+    txt = text.replace("_", " ")
+    return f'Python {txt}'
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def fourth(n):
+    """ Route function for the root URL ('/number/<n>') """
+    if isinstance(n, int):
+        return f'{n} is a number'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
